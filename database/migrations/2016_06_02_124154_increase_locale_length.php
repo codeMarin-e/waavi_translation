18a2ed9c977b25417c1fac4b39c24de02f25d301
Marin <marin@dev.nddesign.no>
2019-10-30 15:01:19 +0200

    first commit

---
+++ database/migrations/2016_06_02_124154_increase_locale_length.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+
+class IncreaseLocaleLength extends Migration
+{
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('translator_translations', function ($table) {
+            $table->string('locale', 10)->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+
+}
---
+++ src/Models/Translation.php
@@ -0,0 +1,54 @@
+<?php namespace Waavi\Translation\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Translation extends Model
+{
+    /**
+     *  Table name in the database.
+     *  @var string
+     */
+    protected $table = 'translator_translations';
+
+    /**
+     *  List of variables that can be mass assigned
+     *  @var array
+     */
+    protected $fillable = ['locale', 'namespace', 'group', 'item', 'text', 'unstable'];
+
+    /**
+     *  Returns the full translation code for an entry: namespace.group.item
+     *  @return string
+     */
+    public function getCodeAttribute()
+    {
+        return $this->namespace === '*' ? "{$this->group}.{$this->item}" : "{$this->namespace}::{$this->group}.{$this->item}";
+    }
+
+    /**
+     *  Flag this entry as Reviewed
+     *  @return void
+     */
+    public function flagAsReviewed()
+    {
+        $this->unstable = 0;
+    }
+
+    /**
+     *  Set the translation to the locked state
+     *  @return void
+     */
+    public function lock()
+    {
+        $this->locked = 1;
+    }
+
+    /**
+     *  Check if the translation is locked
+     *  @return boolean
+     */
+    public function isLocked()
+    {
+        return (boolean) $this->locked;
+    }
+}
